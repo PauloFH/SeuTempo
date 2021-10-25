22595523afb989edb425399682ee827fca883d33
Paulo Roberto Fernandes Holanda <paulofernandes099@gmail.com>
2021-10-25 16:47:52 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SeuTempo',
+    version='1.0.3',
+    packages=[''],
+    url='',
+    license='',
+    author='Paulo Roberto e Arthur Peixoto',
+    author_email='paulorfholanda@gmail.com',
+    description='Trabalho de Fundamentos de calculo '
+)
---
+++ variaveis.py
@@ -0,0 +1,10 @@
+hist = "historia"
+geo = "geografia"
+filo = "filosofia"
+bil = "biologia"
+quim = "quimica"
+fis = "fisica"
+port = "portugues"
+mat = "matematica"
+
+
